3ed58f6eb893fc26e19e18b729f39702b484d138
Uggla <uggla@free.fr>
2019-05-12 20:32:02 +0200

    Add word count exercise

---
+++ word-count/src/lib.rs
@@ -0,0 +1,7 @@
+fn countWords(mut hashMap: HashMap<String, u32>, word: String) -> HashMap<String, u32> {
+    unimplemented!()
+}
+
+pub fn word_count(sentence: &str) -> HashMap<String, u32> {
+    unimplemented!()
+}
