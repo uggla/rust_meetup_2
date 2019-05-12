b76800e6598dc02f0a30871b540fc6612329642a
Uggla <uggla@free.fr>
2019-05-12 20:26:54 +0200

    A solution to proverb

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,7 +1,17 @@
 fn make_verse(words: &[&str]) -> String {
-    unimplemented!()
+    format!("For want of a {} the {} was lost.\n", words[0], words[1])
 }
 
 pub fn build_proverb(list: Vec<&str>) -> String {
-    unimplemented!()
+    let verses = list.windows(2)
+        .map(make_verse)
+        .collect::<String>();
+
+    let ending = match list.len() {
+        0 => "",
+        l if l > 2 => "And all for the want of a horseshoe nail.",
+        _ => "And all for the want of a nail.",
+    };
+
+    format!("{}{}", verses, ending)
 }
