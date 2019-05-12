0ca110f806d9d42ff3f1276725f24020f4c5b87f
Uggla <uggla@free.fr>
2019-05-12 17:47:09 +0200

    Add solution

--- palindromes/src/main.rs
+++ palindromes/src/main.rs
@@ -1,20 +1,35 @@
 use rayon::prelude::*;
 
 fn est_palindrome(phrase: &str) -> bool {
-    unimplemented!()
+    let rev_phrase: String = phrase.chars().into_iter().rev().collect();
+    phrase.to_lowercase().replace(" ", "") == rev_phrase.to_lowercase().replace(" ", "")
 }
 
 fn tous_les_palindromes(phrases: &str) -> Vec<String> {
-    unimplemented!()
+    let palindromes: Vec<String> = phrases
+        .split(".")
+        .map(|x| x.to_string())
+        .filter(|x| est_palindrome(x) && !x.is_empty())
+        .collect();
+    palindromes
 }
 
 fn tous_les_palindromes_en_parallele(phrases: &str) -> Vec<String> {
-    unimplemented!()
+    let palindromes: Vec<String> = phrases
+        .split(".")
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+        .into_par_iter()
+        .filter(|x| est_palindrome(x) && !x.is_empty())
+        .collect();
+    palindromes
 }
 
 fn main() {
     assert!(est_palindrome("un radar nu"));
     assert!(est_palindrome("Un radar nu"));
+    assert!(est_palindrome("Un roc cornu"));
+    assert!(!est_palindrome("Toto"));
     assert_eq!(
         tous_les_palindromes("Un roc cornu. Foo Bar Baz. Un radar nu."),
         vec!["Un roc cornu", " Un radar nu"]
